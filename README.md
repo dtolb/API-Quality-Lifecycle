39519b0ad9e72e7bef4f0ba1b4a26d5a9462722f
Dan Tolbert <dantolbe@cisco.com>
2023-08-11 15:10:07 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IMPACT23-API-Lifecycle
+
 Demo &amp; Workshop for Cisco'23 IMPACT API-Lifecycle
--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -3,17 +3,26 @@ import { Pet, createPet, getPets, getPetById } from '../models/Pet';
 
 @Route('pets')
 export class PetController {
-  @Get('/')
+  @Get()
+  @Tags('Pets')
+  @Description('Retrieve a list of all pets.')
+  @OperationId('listPets')
   public async getPets(): Promise<Pet[]> {
     return getPets();
   }
 
   @Get('{id}')
+  @Tags('Pets')
+  @Description('Retrieve a specific pet by its id.')
+  @OperationId('getPet')
   public async getPet(@Path() id: number): Promise<Pet | null> {
     return getPetById(id) || null;
   }
 
-  @Post('/')
+  @Post()
+  @Tags('Pets')
+  @Description('Create a new pet.')
+  @OperationId('createPet')
   public async createPet(@BodyProp() name: string): Promise<Pet> {
     return createPet(name);
   }
