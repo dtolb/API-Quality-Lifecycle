9b669dcec553c4d845eb9117ee6aa53f7a8d7e22
Dan Tolbert <dantolbe@cisco.com>
2023-08-26 21:11:34 -0400

    update spec gen

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
 import express from 'express';
+// import { ValidateError } from 'tsoa';
 import bodyParser from 'body-parser';
 import { RegisterRoutes } from './routes/routes';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
 // Register routes
 RegisterRoutes(app);
+
+// app.use(function notFoundHandler(_req, res: ExResponse) {
+//   res.status(404).send({
+//     message: 'Not Found',
+//   });
+// });
+
+// app.use(function errorHandler(err: unknown, req: ExRequest, res: ExResponse, next: NextFunction): ExResponse | void {
+//   if (err instanceof ValidateError) {
+//     console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+//     return res.status(422).json({
+//       message: 'Validation Failed',
+//       details: err?.fields,
+//     });
+//   }
+//   if (err instanceof Error) {
+//     return res.status(500).json({
+//       message: 'Internal Server Error',
+//     });
+//   }
+//
+//   next();
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -1,25 +1,131 @@
-export interface Pet {
+/**
+ * @tsoaModel
+ * @example
+ * {
+ *   items: [
+ *     {
+ *       "id": 1,
+ *       "name": "Fargo",
+ *       "species": "dog"
+ *     },
+ *     {
+ *       "id": 2,
+ *       "name": "June",
+ *       "species": "dog"
+ *     }
+ *   ]
+ * }
+ */
+export interface PetsList {
+  /**
+   * The list of pets
+   */
+  items: Pet[];
+}
+
+/**
+ * @tsoaModel
+ * @example
+ * {
+ *   "id": 2,
+ *   "name": "Fargo",
+ *   "species": "dog"
+ * }
+ */
+export interface Pet extends CreatePetRequest {
+  /**
+   * The id of the pet, used for update and fetching
+   */
   id: number;
+}
+
+/**
+ * @tsoaModel
+ * @example
+ * {
+ *   "max": 2
+ *   "sort": 'asc',
+ *   "offset": '29523'
+ * }
+ */
+export interface PaginationRequest {
+  /**
+   * Maximum number of items to return
+   */
+  max?: number;
+  /**
+   * Sorting order
+   */
+  order?: 'asc' | 'desc';
+  /**
+   * offset id to get next page
+   */
+  offset?: string;
+}
+
+/**
+ * @tsoaModel
+ * @example
+ * {
+ *  "name": "Fargo",
+ *  "species": "dog"
+ * }
+ */
+export interface CreatePetRequest extends UpdatePetRequest {
+  /**
+   * The species of the pet
+   */
+  species: string;
+}
+
+/**
+ * @tsoaModel
+ * @example
+ * {
+ *   "name": "Dolph"
+ * }
+ */
+export interface UpdatePetRequest {
+  /**
+   * The name of the pet, can be updated later
+   */
   name: string;
 }
 
-let pets: Pet[] = [];
+//in memory db for now
+export let pets: Pet[] = [];
 let nextId = 1;
 
-export function createPet(name: string): Pet {
+export function createPet(createPetRequest: CreatePetRequest): Pet {
   const newPet = {
     id: nextId++,
-    name,
+    name: createPetRequest.name,
+    species: createPetRequest.species,
   };
 
   pets.push(newPet);
   return newPet;
 }
 
+export function updatePet(id: number, updatePetRequest: UpdatePetRequest): Pet | null {
+  const index = pets.findIndex((p: Pet) => p.id === id);
+  if (index === -1) {
+    return null;
+  }
+
+  const existingPet = pets[index];
+  existingPet.name = updatePetRequest.name ? updatePetRequest.name : existingPet.name;
+  return existingPet;
+}
+
 export function getPetById(id: number): Pet | undefined {
   return pets.find((pet) => pet.id === id);
 }
 
-export function getPets(): Pet[] {
-  return pets;
+export function getPets(pagination: PaginationRequest): PetsList {
+  if (pagination) {
+  }
+  return {
+    items: pets,
+  };
 }
